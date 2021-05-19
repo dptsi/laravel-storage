2e8f980908ac92c5837b375f8f55d5028e0ed7dd
ersadishla <ersadishlahuddin@gmail.com>
2021-05-19 19:41:58 +0700

    update config filestorage

--- src/config/filestorage.php
+++ src/config/filestorage.php
@@ -1,9 +1,8 @@
 <?php
 
-// config file storage
 return [
     'base_uri'          => env('FILE_STORAGE_BASE_URI'),
-    'myits_uri'        => env('FILE_STORAGE_MYITS_URI'),
+    'myits_uri'         => env('FILE_STORAGE_MYITS_URI'),
     'client_id'         => env('FILE_STORAGE_CLIENT_ID'),
     'client_secret'     => env('FILE_STORAGE_CLIENT_SECRET'),
 ];
