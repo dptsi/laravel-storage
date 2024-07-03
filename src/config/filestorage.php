3b55af186e59a29255f847678cd442ad119fd7db
Ali Mangsur <ali@its.ac.id>
2024-07-03 15:34:34 +0700

    add GCS support

--- src/config/filestorage.php
+++ src/config/filestorage.php
@@ -10,4 +10,8 @@ return [
     'aws_secret'                => env('AWS_SECRET_ACCESS_KEY'),
     'aws_region'                => env('AWS_DEFAULT_REGION'),
     'aws_bucket'                => env('AWS_BUCKET'),
+    //config gcs
+    'gcs_key_path'              => env('GOOGLE_KEY_PATH'),
+    'gcs_project_id'            => env('GOOGLE_PROJECT_ID'),
+    'gcs_bucket'                => env('GOOGLE_BUCKET'),
 ];
